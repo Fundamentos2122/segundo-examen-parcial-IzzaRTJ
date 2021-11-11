ee2516f16ff6e3b61259ed0cac5e6864d2d6c608
= <=>
2021-11-10 23:42:23 -0600

    parcial 2 completo

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const modal = document.getElementById("modal");
 const task_space = document.getElementById("tasks-space")
 const openbtn = document.getElementById("btn-nuevo");
 const see_all = document.getElementById("see-all");
+const close_btn = document.getElementById("modal-close-btn");
+const newtask_form = document.getElementById("new-task");
 
 var keychain = [];
 var task_list = [];
@@ -11,18 +13,12 @@ var task_list = [];
 const attr_toggle = "data-toggle";
 const attr_target = "data-target";
 const attr_dismiss = "data-dismis";
-
 const class_modal = "modal";
 const class_task = "task"
 const class_show = "show";
 const class_hide = "hide";
 const class_complete = "complete";
 const name_complete = "complete";
-
-
-var close_btn = document.getElementById("modal-close-btn");
-
-const newtask_form = document.getElementById("new-task");
 
 document.addEventListener("DOMContentLoaded",function(){
 
@@ -48,6 +44,11 @@ document.addEventListener("DOMContentLoaded",function(){
     see_all.addEventListener("click",function(){showAll(see_all);});
 
 })
+/**
+ * funcion que edita una tarea a completada, buscara el elemento perenenciente al identificador en attr_target del checkbox
+ * agregando la clase complete. ocultara la tarea si es posible.
+ * @param {checkbox} box 
+ */
 function acomplete(box)
 {
     console.log(box);
@@ -65,19 +66,22 @@ function acomplete(box)
         tasktarget.classList.remove(class_complete);
         tasktarget.classList.remove(class_hide);
     }
+    updateLStask(tasktarget,box.checked);
 }
 
-function showAll(box){
+/**
+ * muestra todas las tareas, incluyendo las que se completaron
+ * @param {checkbox} box 
+ */
+function showAll(box){  
     task_list.forEach(element => {
         if(box.checked)
             element.classList.remove(class_hide);
         else{  
             if(element.classList.contains(class_complete))
                 element.classList.add(class_hide);
-
         }
     });
-
 }
 
 /**
@@ -106,6 +110,9 @@ function openModal(e){
     modal.clasdList.remove(class_modal)
 };
 
+/**
+ * carga un llavero del localstorage para cargar todas las tareas guardadas.
+ */
 function loadTasks(){
 
     let keys = localStorage.getItem(LS_keyChain);
@@ -117,14 +124,14 @@ function loadTasks(){
     keychain.forEach(task_key =>{
         let task_json = localStorage.getItem(task_key);
         let task_data = JSON.parse(task_json);
-        console.log(task_data);
         addToList(task_data);
-        
-        //funcion para cargar las tareas
     });
 
 };
 
+/**
+ * Regresa el  formulario de nueva tarea a un estado por defecto
+ */
 function emptyForm(){
     newtask_form["title"].value = null;
     newtask_form["description"].value = null;
@@ -133,7 +140,8 @@ function emptyForm(){
 }
 
 /**
- * guarda los datos en localStorage y agrega la nueva tarea a la pantalla
+ * Toma los datos del formulario y los usa para crear un objeto, el cual guardara la informacion relevante
+ * de una tarea en el localstorage.
  * @param {PointerEvent} e 
  */
 function saveNewTask(e){
@@ -157,17 +165,13 @@ function saveNewTask(e){
 
     //agregamos la nueva llave al arreglo de llaves actual y guardamos el llavero en el localstorage
     keychain.push(data.id);
-    update_keychain();
+    var keyjson = JSON.stringify(keychain);
+    localStorage.setItem(LS_keyChain,keyjson);
 
     addToList(data);
     //cerramos  el modal
     closeModal();
 };
-
-function update_keychain(){
-    var keyjson = JSON.stringify(keychain);
-    localStorage.setItem(LS_keyChain,keyjson);
-}
 
 /**
  * funcion para crear una llave apartir de la fecha actual, regresa una cadena
@@ -185,13 +189,19 @@ function dateToKey(){
 }
 
 /**
- * funcion para agregar la nueca tarea a la lista de tareas
+ * funcion para agregar una tarea, creada o cargada a la pagina
  * @param {Object} data 
  */
 function addToList(data)
 {
     newtask = document.createElement("div");
     newtask.className = "col-12 task";
+    let check = null;
+    if(data.complete){
+        newtask.classList.add(class_complete,class_hide);
+        check = "checked";
+    }
+        
     newtask.id = data.id;
     newtask.innerHTML = `
         <div class="row">
@@ -205,11 +215,24 @@ function addToList(data)
                 ${data.description}
             </div>
             <div class="col-12 toLeft">
-                <input type="checkbox" name="checkbox" data-toggle="task" data-target="${data.id}">
+                <input type="checkbox" name="checkbox" ${check} data-toggle="task" data-target="${data.id}">
                 <label for="checkbox">Completa</label>
             </div>
         </div>`
     task_list.push(newtask);
     task_space.appendChild(newtask);
     
+}
+
+/**
+ * funcion para actualizar los datos en el localstorage cuando una tarea se completa o se cambia.
+ * @param {Object} data la informacion del elemento
+ * @param {boolean} checked un booleano acorde al checkbox del elemento
+ */
+function updateLStask(data,checked){
+    let djson = JSON.parse(localStorage.getItem(data.id));
+    djson.complete = checked;
+    let backjson = JSON.stringify(djson);
+    localStorage.setItem(data.id,backjson);
+
 }
